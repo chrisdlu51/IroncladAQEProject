59a9515b137a309aabc22e175264ba14181573af
Chris Lu <chris.lu94@gmail.com>
2025-03-23 13:04:09 -0700

    added mochawesome reporting

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from 'cypress';
+import fs from 'fs';
 
 export default defineConfig({
   e2e: {
     baseUrl: "https://www.saucedemo.com/", // Change this as needed
     specPattern: "cypress/**/**/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
-      // Implement node event listeners here
+
+    },
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: true
     },
     watchForFileChanges: false
+
   },
 });
