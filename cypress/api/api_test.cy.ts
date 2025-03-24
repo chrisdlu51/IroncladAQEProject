10c33e09d9ead807301ad3c4f338a13ab5caa3d9
Chris Lu <chris.lu94@gmail.com>
2025-03-24 15:39:25 -0700

    added cypress github actions workflow

--- cypress/api/api_test.cy.ts
+++ cypress/api/api_test.cy.ts
@@ -8,12 +8,16 @@ describe("API Test - JSONPlaceholder", () => {
         expect(response.status).to.eq(200);
         expect(response.body).to.be.an("array");
         expect(response.body.length).to.be.greaterThan(0);
+        cy.log(response.body);
+        for (var v in response.body[0]) {
+          cy.log(v, response.body[0][v]);
+        }
       });
     });
   
     it("POST /posts should create a new post", () => {
       cy.request("POST", `${baseUrl}/posts`, {
-        title: "Cypress Test Post",
+        title: "Cypress test post",
         body: "This is a test post created using Cypress.",
         userId: 1,
       }).then((response) => {
@@ -25,12 +29,12 @@ describe("API Test - JSONPlaceholder", () => {
     it("PUT /posts/1 should update a post", () => {
       cy.request("PUT", `${baseUrl}/posts/1`, {
         id: 1,
-        title: "Updated Post Title",
+        title: "Updated post title",
         body: "Updated post content.",
         userId: 1,
       }).then((response) => {
         expect(response.status).to.eq(200);
-        expect(response.body.title).to.eq("Updated Post Title");
+        expect(response.body.title).to.eq("Updated post title");
       });
     });
   
