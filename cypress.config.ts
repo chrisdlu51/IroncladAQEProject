e3abd35eb88c4793db49c82024440ef892270dfe
Chris Lu <chris.lu94@gmail.com>
2025-03-23 19:51:00 -0700

    added log management to cypress.config.ts file

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'cypress';
 import fs from 'fs';
+import path from 'path';
 
 export default defineConfig({
   e2e: {
     baseUrl: "https://www.saucedemo.com/", // Change this as needed
     specPattern: "cypress/**/**/*.cy.{js,jsx,ts,tsx}",
-    setupNodeEvents(on, config) {
-
-    },
     reporter: 'mochawesome',
     reporterOptions: {
       reportDir: 'cypress/reports',
@@ -15,7 +13,27 @@ export default defineConfig({
       html: true,
       json: true
     },
-    watchForFileChanges: false
+    watchForFileChanges: false,
+    setupNodeEvents(on, config) {
+      on('before:run', () => {
+        const reportDir = 'cypress/reports';
+        const maxReports = 5;
 
-  },
+        // this deletes old log reports
+        if (fs.existsSync(reportDir)) {
+          let files = fs.readdirSync(reportDir)
+            .filter(file => file.endsWith('.json') || file.endsWith('.html'))
+            .map(file => ({ file, time: fs.statSync(path.join(reportDir, file)).mtime.getTime() }))
+            .sort((a, b) => b.time - a.time); 
+
+          if (files.length > maxReports) {
+            files.slice(maxReports).forEach(({ file }) => {
+              fs.unlinkSync(path.join(reportDir, file));
+            });
+          }
+        }
+    });
+
+    }
+  }
 });
