1a2d875a572b97c216cf1988ec3579e43b6d1e65
Chris Lu <chris.lu94@gmail.com>
2025-03-24 19:54:49 -0700

    disabled mochawesome reporter

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,13 +7,13 @@ export default defineConfig({
     baseUrl: 'https://www.saucedemo.com/', // Change this as needed
     projectId: 'ytnthi',
     specPattern: 'cypress/**/**/*.cy.{js,jsx,ts,tsx}',
-    reporter: 'mochawesome',
-    reporterOptions: {
-      reportDir: 'cypress/reports',
-      overwrite: false,
-      html: true,
-      json: true
-    },
+    // reporter: 'mochawesome',
+    // reporterOptions: {
+    //   reportDir: 'cypress/reports',
+    //   overwrite: false,
+    //   html: true,
+    //   json: true
+    // },
     watchForFileChanges: false,
     setupNodeEvents(on, config) {
       const MAX_LOG_LINES = 1000;
@@ -56,24 +56,24 @@ export default defineConfig({
           return null;
         }
       });
-      on('before:run', () => {
-        const reportDir = 'cypress/reports';
-        const maxReports = 5;
+      // on('before:run', () => {
+      //   const reportDir = 'cypress/reports';
+      //   const maxReports = 5;
 
-        // this deletes old log reports
-        if (fs.existsSync(reportDir)) {
-          let files = fs.readdirSync(reportDir)
-            .filter(file => file.endsWith('.json') || file.endsWith('.html'))
-            .map(file => ({ file, time: fs.statSync(path.join(reportDir, file)).mtime.getTime() }))
-            .sort((a, b) => b.time - a.time); 
+      //   // this deletes old log reports
+      //   if (fs.existsSync(reportDir)) {
+      //     let files = fs.readdirSync(reportDir)
+      //       .filter(file => file.endsWith('.json') || file.endsWith('.html'))
+      //       .map(file => ({ file, time: fs.statSync(path.join(reportDir, file)).mtime.getTime() }))
+      //       .sort((a, b) => b.time - a.time); 
 
-          if (files.length > maxReports) {
-            files.slice(maxReports).forEach(({ file }) => {
-              fs.unlinkSync(path.join(reportDir, file));
-            });
-          }
-        }
-    });
+      //     if (files.length > maxReports) {
+      //       files.slice(maxReports).forEach(({ file }) => {
+      //         fs.unlinkSync(path.join(reportDir, file));
+      //       });
+      //     }
+      //   }
+      // });
 
     }
   }
